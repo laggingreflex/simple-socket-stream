aadcc5dd5cd6e3e096d8c0fd1e237834567baf53
laggingreflex <laggingreflex@gmail.com>
2017-05-11 13:41:13 +0530

    add opts, fix passing args

--- index.js
+++ index.js
@@ -1,26 +1,47 @@
-const ss = require('socket.io-stream');
+'use strict';
 
-module.exports = (socket, send, receive) => {
+const ss = require('socket.io-stream');
+const merge = require('merge');
+
+module.exports = simpleSocketStream;
+
+const defaults = {
+  messages: {
+    sendFile: 'send-file',
+    streamFile: 'stream-file',
+  }
+}
+
+simpleSocketStream.defaults = merge({}, defaults);
+
+function simpleSocketStream(socket, send, receive, opts) {
+  if (!socket) {
+    throw new Error('Need a socket instance');
+  }
+  if (!send || typeof send !== 'function') {
+    throw new Error('Need a send callback function');
+  }
+  if (!receive || typeof receive !== 'function') {
+    throw new Error('Need a receive callback function');
+  }
+
+  opts = merge({}, defaults, simpleSocketStream.defaults, opts);
+
   const streamSocket = ss(socket);
 
-  // 2. receiver upon seeing this request,
-  // creates a writable socket stream to which the sender can now pipe
-  // and sends this steam object to the sender
-  socket.on('send-file', data => {
+  socket.on(opts.messages.sendFile, function(data) {
     const stream = ss.createStream();
-    streamSocket.emit('stream-file', stream, data);
-
-    // 4. receiver can take this stream and write it to a file
-    receive(stream, data);
+    const args = [stream].concat([].slice.call(arguments));
+    streamSocket.emit.apply(streamSocket, [opts.messages.streamFile].concat(args));
+    return receive.apply(null, args);
   });
 
-
-  // 3. sender now has a channel on which he can push whatever he wanted to send
-  streamSocket.on('stream-file', (stream, data) => {
-    send(stream, data)
+  streamSocket.on(opts.messages.streamFile, function(stream, data) {
+    send.apply(null, arguments);
   });
 
-
-  // 1. sender wants to send a file - he just calls this function
-  return data => socket.emit('send-file', data)
+  return function initiate(data) {
+    const args = [opts.messages.sendFile].concat([].slice.call(arguments));
+    return socket.emit.apply(socket, args);
+  };
 }
